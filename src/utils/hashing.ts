d3bd6f8ad0aa4d4d5b320920d24135d149e1c126
bellanzo <abimbolaomisakin678@gmail.com>
2025-09-26 03:09:50 +0100

    fixed some issues

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -14,11 +14,6 @@ export const doHashValidation = (
   return result;
 };
 
-export const hmacProcess = (value: any, key: any) => {
-  const result = createHmac("sha256", key).update(value).digest("hex");
-  return result;
-};
-
 export const hashToken = (token: string): string => {
   return createHash("sha256").update(token).digest("hex");
 };
